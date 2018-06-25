974a257c27002ac2786c6eb210437e42af5365ef
dandanli814 <sdldd520@163.com>
2018-06-25 17:20:41 +0800

    8

---
+++ FindNextTreeNode.cpp
@@ -0,0 +1,164 @@
+#include <stdio.h>
+
+struct BinaryTreeNode
+{
+    int                    m_nValue;
+    BinaryTreeNode*        m_pLeft;
+    BinaryTreeNode*        m_pRight;
+    BinaryTreeNode*        m_pParent;
+};
+
+BinaryTreeNode* GetNext(BinaryTreeNode* pNode)
+{
+    if(pNode == NULL){
+    	return NULL;
+    }
+    BinaryTreeNode* pNext = pNode;
+    if(pNode -> m_pRight != NULL){
+    	BinaryTreeNode* pRight = pNode -> m_pRight;
+    	while(pRight -> m_pLeft != NULL){
+    		pRight = pRight -> m_pLeft;
+    	}
+    	pNext = pRight;
+    	return pNext;
+    }
+    else if(pNode -> m_pRight == NULL && pNode == pNode -> m_pParent -> m_pLeft){
+    	pNext = pNode -> m_pParent;
+    	return pNext;
+    }
+    else if(pNode -> m_pRight == NULL && pNode == pNode -> m_pParent -> m_pRight){
+    	BinaryTreeNode* pParent = pNode -> m_pParent;
+    	BinaryTreeNode* pCurrent = pNode;
+    	while(pParent != NULL &&  pCurrent == pParent -> m_pRight){
+    		pCurrent = pParent;
+    		pParent = pParent -> m_pParent;
+    	}
+    	pNext = pParent;
+    }
+    return pNext;
+}
+
+// ==================== ���������������������� ====================
+BinaryTreeNode* CreateBinaryTreeNode(int value)
+{
+    BinaryTreeNode* pNode = new BinaryTreeNode();
+    pNode->m_nValue = value;
+    pNode->m_pLeft = NULL;
+    pNode->m_pRight = NULL;
+    pNode->m_pParent = NULL;
+
+    return pNode;
+}
+
+void ConnectTreeNodes(BinaryTreeNode* pParent, BinaryTreeNode* pLeft, BinaryTreeNode* pRight)
+{
+    if(pParent != NULL)
+    {
+        pParent->m_pLeft = pLeft;
+        pParent->m_pRight = pRight;
+
+        if(pLeft != NULL)
+            pLeft->m_pParent = pParent;
+        if(pRight != NULL)
+            pRight->m_pParent = pParent;
+    }
+}
+
+void PrintTreeNode(BinaryTreeNode* pNode)
+{
+    if(pNode != NULL)
+    {
+        printf("value of this node is: %d\n", pNode->m_nValue);
+
+        if(pNode->m_pLeft != NULL)
+            printf("value of its left child is: %d.\n", pNode->m_pLeft->m_nValue);
+        else
+            printf("left child is null.\n");
+
+        if(pNode->m_pRight != NULL)
+            printf("value of its right child is: %d.\n", pNode->m_pRight->m_nValue);
+        else
+            printf("right child is null.\n");
+    }
+    else
+    {
+        printf("this node is null.\n");
+    }
+
+    printf("\n");
+}
+
+void PrintTree(BinaryTreeNode* pRoot)
+{
+    PrintTreeNode(pRoot);
+
+    if(pRoot != NULL)
+    {
+        if(pRoot->m_pLeft != NULL)
+            PrintTree(pRoot->m_pLeft);
+
+        if(pRoot->m_pRight != NULL)
+            PrintTree(pRoot->m_pRight);
+    }
+}
+
+void DestroyTree(BinaryTreeNode* pRoot)
+{
+    if(pRoot != NULL)
+    {
+        BinaryTreeNode* pLeft = pRoot->m_pLeft;
+        BinaryTreeNode* pRight = pRoot->m_pRight;
+
+        delete pRoot;
+        pRoot = NULL;
+
+        DestroyTree(pLeft);
+        DestroyTree(pRight);
+    }
+}
+
+// ====================���Դ���====================
+void Test(char* testName, BinaryTreeNode* pNode, BinaryTreeNode* expected)
+{
+    if(testName != NULL)
+        printf("%s begins: ", testName);
+
+    BinaryTreeNode* pNext = GetNext(pNode);
+    if(pNext == expected)
+        printf("Passed.\n");
+    else
+        printf("FAILED.\n");
+}
+
+//            8
+//        6      10
+//       5 7    9  11
+void Test1_7()
+{
+    BinaryTreeNode* pNode8 = CreateBinaryTreeNode(8);
+    BinaryTreeNode* pNode6 = CreateBinaryTreeNode(6);
+    BinaryTreeNode* pNode10 = CreateBinaryTreeNode(10);
+    BinaryTreeNode* pNode5 = CreateBinaryTreeNode(5);
+    BinaryTreeNode* pNode7 = CreateBinaryTreeNode(7);
+    BinaryTreeNode* pNode9 = CreateBinaryTreeNode(9);
+    BinaryTreeNode* pNode11 = CreateBinaryTreeNode(11);
+
+    ConnectTreeNodes(pNode8, pNode6, pNode10);
+    ConnectTreeNodes(pNode6, pNode5, pNode7);
+    ConnectTreeNodes(pNode10, pNode9, pNode11);
+
+    Test("Test1", pNode8, pNode9);
+    Test("Test2", pNode6, pNode7);
+    Test("Test3", pNode10, pNode11);
+    Test("Test4", pNode5, pNode6);
+    Test("Test5", pNode7, pNode8);
+    Test("Test6", pNode9, pNode10);
+    Test("Test7", pNode11, NULL);
+
+    DestroyTree(pNode8);
+}
+
+int main(int argc, char* argv[])
+{
+    Test1_7();
+}
